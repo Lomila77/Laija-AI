a6a04e9b1e1cb1fcde70e88b02ae7fdd9d5ac9d6
garance <garance@jimini.ai>
2024-05-16 22:59:34 +0200

    A user message class formats message for IA can easily understand it

---
+++ packages/ai/llm/utils/messages/user_messages.py
@@ -0,0 +1,28 @@
+
+from pydantic import BaseModel, field_validator
+
+class UserMessage(BaseModel):
+    message: str
+
+    @field_validator("message")
+    @classmethod
+    def validate_message(cls, message) -> str:
+        if message is None or len(message) == 0:
+            raise ValueError("Invalid message")
+        return message
+
+    def format_message(self) -> str:
+        return "Utilisateur : " + self.message
+
+class AssistantMessage(BaseModel):
+    message: str
+
+    @field_validator("message")
+    @classmethod
+    def validate_message(cls, message) -> str:
+        if message is None or len(message) == 0:
+            raise ValueError("Invalid message")
+        return message
+
+    def format_message(self) -> str:
+        return "Assistant : " + self.message
